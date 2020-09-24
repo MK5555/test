97e345164d239692b64ef1f7eb57b3c9b36e477f
us <rf.rvr@yandex.ru>
2020-09-24 10:48:12 +0300

    add basic 2.4

---
+++ 002_basic/2.js
@@ -0,0 +1,55 @@
+
+// переменные объявляются через let
+"use strict";
+let message;
+message = "hello";
+//const
+//есть константы которе вычисляются во время выполения
+//const = pageLoadTime = например
+const pi = 3.1416
+console.log(message)
+console.log(pi)
+
+//task 1
+let admin = "Джон";
+let name;
+name = admin;
+console.log(name)
+//task 2
+let ourPlanetName = "EARTH"
+const currentUserName="";
+
+//task 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
